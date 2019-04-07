1c23af386e6621f35656c48a52723f9b41709e9c
IgorSily <igorsily2@gmail.com>
2019-04-07 01:49:25 -0300

    normalizando porta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const debug = require("debug")("nodestore:server");
 
 
 const app = express();
-const port = 3001;
+const port = normalizePort(process.env.PORT || "3001");
 
 const server = http.createServer(app);
 const router = express.Router();
@@ -12,3 +12,19 @@ const router = express.Router();
 server.listen(port, () => {
     console.log("Server UP");
 })
+
+normalizePort = port => {
+
+    const port = parseInt(port, 10);
+
+    if (isNaN(port)) {
+        return port;
+    }
+
+    if (port => 0) {
+        return port;
+    }
+
+    return false;
+
+}
